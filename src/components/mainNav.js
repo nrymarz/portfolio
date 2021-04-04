b02836e9be1f598c0b8de103186298f30c5e5f9c
Nathan Rymarz <nrymarz@gmail.com>
2021-04-03 20:02:25 -0500

    added links to important sites in navbar and a bit of styling

--- src/components/mainNav.js
+++ src/components/mainNav.js
@@ -1,14 +1,18 @@
 import Navbar from 'react-bootstrap/Navbar'
-import Row from 'react-bootstrap/Row'
-import Container from 'react-bootstrap/Container'
-import dev from '../assets/icons/devIcon.png'
+import Nav from 'react-bootstrap/Nav'
+import dev from '../assets/icons/dev.png'
+import github from '../assets/icons/github.png'
+import linkedIn from '../assets/icons/linkedIn.png'
 
 export default function MainNav(){
     return(
-        <Container>
-            <Navbar bg='dark' variant="dark" fixed='top'>
-                <Navbar.Brand>Nathan Rymarz</Navbar.Brand>
-            </Navbar>
-        </Container>
+        <Navbar variant="dark" fixed='top' style={{background:"#505050"}}>
+            <Navbar.Brand>Nathan Rymarz</Navbar.Brand>
+            <Nav className="ml-auto">
+            <Nav.Link href="https://github.com/nrymarz" target="_blank"><img src={github} width='50' height='50'/></Nav.Link>
+            <Nav.Link href="https://www.linkedin.com/in/nathan-rymarz/" target="_blank"><img src={linkedIn} width='50' height='50'/></Nav.Link>
+            <Nav.Link href="https://dev.to/nrymarz" target="_blank"><img src={dev} width='50' height='50'/></Nav.Link>
+            </Nav>
+        </Navbar>
     )
 }
