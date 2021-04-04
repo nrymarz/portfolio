768de01430e698d3c3b72eebbb368d674a328b4b
Nathan Rymarz <nrymarz@gmail.com>
2021-04-03 21:54:39 -0500

    added state with objects for each project to projects.js

--- src/containers/blogPosts.js
+++ src/containers/blogPosts.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+
 export default class BlogPosts extends Component{
     render(){
         return(
--- src/containers/projects.js
+++ src/containers/projects.js
@@ -1,5 +1,32 @@
 import React, {Component} from 'react'
+import Project from '../components/project.js'
+import vgPic from '../assets/project_pics/vg_backlog_tracker_pic.png'
+import streamPic from '../assets/project_pics/random_stream_finder_pic.png'
+import rpgPic from '../assets/project_pics/dragon_adventure_pic.png'
+
 export default class Projects extends Component{
+    state={
+        projects:{
+            dragonAdventure:{
+                pic: rpgPic,
+                site: "https://dragon-adventure.herokuapp.com/",
+                demo: "https://www.youtube.com/watch?v=ZhbnOaCcE2E",
+                github: "https://github.com/nrymarz/js-dragon-rpg"
+            },
+            vgBacklogTracker:{
+                pic: vgPic,
+                site: "https://guarded-temple-25512.herokuapp.com/",
+                demo: "https://www.youtube.com/watch?v=pkllnDsc7iU",
+                github: "https://github.com/nrymarz/vg-backlog-tracker"
+            },
+            randomStreamFinder:{
+                pic: streamPic,
+                site: "https://randomstreamfinder.herokuapp.com/",
+                demo: "https://www.youtube.com/watch?v=EoeyYE8D6IU",
+                github: "https://github.com/nrymarz/Twitch-Random-Stream-Finder"
+            }
+        }
+    }
     render(){
         return(
             <>
