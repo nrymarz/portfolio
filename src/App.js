d90c086ec4dda5358a39346741483f66ba60a0e5
Nathan Rymarz <nrymarz@gmail.com>
2021-04-03 19:32:58 -0500

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import About from './components/about.js'
+import MainNav from './components/mainNav.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <MainNav/>
       <About/>
     </div>
   );
--- src/components/mainNav.js
+++ src/components/mainNav.js
@@ -1,9 +1,14 @@
 import Navbar from 'react-bootstrap/Navbar'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+import dev from '../assets/icons/devIcon.png'
 
 export default function MainNav(){
     return(
-        <Navbar>
-
-        </Navbar>
+        <Container>
+            <Navbar bg='dark' variant="dark" fixed='top'>
+                <Navbar.Brand>Nathan Rymarz</Navbar.Brand>
+            </Navbar>
+        </Container>
     )
 }
