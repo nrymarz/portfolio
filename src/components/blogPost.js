913f034e2b7f2582276145c1ded672d048ec8dff
Nathan Rymarz <nrymarz@gmail.com>
2021-04-05 00:24:54 -0500

    changed blogpost h3 to h2

--- src/components/blogPost.js
+++ src/components/blogPost.js
@@ -2,7 +2,7 @@ import ScrollAnimation from 'react-animate-on-scroll'
 export default function BlogPost({blog, direction}){
     return(
         <ScrollAnimation animateIn={`animate__fadeIn${direction}`}>
-            <h2 className="py-2"><a href={blog.link} target="_blank" rel="noreferrer">{blog.title}</a></h2>
+            <h3 className="py-2"><a href={blog.link} target="_blank" rel="noreferrer">{blog.title}</a></h3>
         </ScrollAnimation>
     )
     
