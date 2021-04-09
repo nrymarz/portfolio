08a0079e23f855f0479f59b3dc7abbd5ea6232e2
Nathan Rymarz <nrymarz@gmail.com>
2021-04-09 17:21:56 -0500

    made height of card scale to vw and vh

--- src/components/project.js
+++ src/components/project.js
@@ -1,8 +1,9 @@
 import Breadcrumb from 'react-bootstrap/Breadcrumb'
 import Card from 'react-bootstrap/Card'
 export default function Project({project}){
+    let height = window.innerHeight*0.4 + 0.2*window.innerWidth
     return(
-        <Card className="mx-auto" style={{background:'#303030', height:"70vh"}}>
+        <Card className="mx-auto" style={{background:'#303030', height: height}}>
             <Card.Img src={project.pic} className="mx-auto pt-3" alt={project.name} style={{height:"35vh",objectFit:'contain', width:"55vw", maxHeight:"30vw"}}/>
             <Card.Body className="d-inline-flex flex-column">
                 <Card.Title className="my-auto">{project.name}</Card.Title>
