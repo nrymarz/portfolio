d9297aac60145cde3eb99a18f24fc9929debd746
Nathan Rymarz <nrymarz@gmail.com>
2021-04-14 14:47:38 -0500

    removed carousel and switched to horizontal card formats for projects

--- src/components/project.js
+++ src/components/project.js
@@ -1,18 +1,30 @@
 import Breadcrumb from 'react-bootstrap/Breadcrumb'
 import Card from 'react-bootstrap/Card'
-export default function Project({project}){
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+export default function Project({project, idx}){
+    function renderCardImage(){
+        return <Col> <Card.Img src={project.pic} alt={project.name} style={{height:"100%", objectFit:"fill"}}/> </Col>
+    }
     return(
-        <Card className="mx-auto" >
-            <Card.Img src={project.pic} className="mx-auto pt-3" alt={project.name} style={{height:'35vh', objectFit:'contain'}}/>
-            <Card.Body className="d-inline-flex flex-column">
-                <Card.Title className="my-auto">{project.name}</Card.Title>
-                <Card.Text>{project.description}</Card.Text>
-                <Breadcrumb className="w-75 mx-auto mt-auto">
-                    <Breadcrumb.Item href={project.site} target="_blank" className="ml-auto">Live Site</Breadcrumb.Item>
-                    <Breadcrumb.Item href={project.demo} target="_blank">Youtube Demo</Breadcrumb.Item>
-                    <Breadcrumb.Item href={project.github} target="_blank" className="mr-auto">Github</Breadcrumb.Item>
-                </Breadcrumb>
-            </Card.Body>
+        <Card className="my-3">
+            <Row noGutters>
+                {idx%2===0 ? renderCardImage() : null}
+                <Col className="d-flex">
+                    <Card.Body className="d-inline-flex flex-column">
+                        <Card.Title className="my-auto">{project.name}</Card.Title>
+                        <br/>
+                        <Card.Text>{project.description}</Card.Text>
+                        <Card.Text>{project.technologies.join(', ')}</Card.Text>
+                        <Breadcrumb className="w-75 mx-auto mt-auto">
+                            <Breadcrumb.Item href={project.site} target="_blank" className="ml-auto">Live Site</Breadcrumb.Item>
+                            <Breadcrumb.Item href={project.demo} target="_blank">Youtube Demo</Breadcrumb.Item>
+                            <Breadcrumb.Item href={project.github} target="_blank" className="mr-auto">Github</Breadcrumb.Item>
+                        </Breadcrumb>
+                    </Card.Body>
+                </Col>
+                {idx%2===1 ? renderCardImage() : null}
+            </Row>
         </Card>
     )
 }
--- src/containers/projects.js
+++ src/containers/projects.js
@@ -4,7 +4,6 @@ import vgPic from '../assets/project_pics/vg_backlog_tracker_pic.png'
 import streamPic from '../assets/project_pics/random_stream_finder_pic.png'
 import rpgPic from '../assets/project_pics/dragon_adventure_pic.png'
 import Container from 'react-bootstrap/Container'
-import Carousel from 'react-bootstrap/Carousel'
 import Col from 'react-bootstrap/Col'
 import ScrollAnimation from 'react-animate-on-scroll'
 
@@ -12,20 +11,23 @@ export default class Projects extends Component{
     state={
         projects:[
             {
-                name: "Dragon Adventure RPG",
-                pic: rpgPic,
-                site: "https://dragon-adventure.herokuapp.com/",
-                demo: "https://www.youtube.com/watch?v=ZhbnOaCcE2E",
-                github: "https://github.com/nrymarz/js-dragon-rpg",
-                description: "This is a simple turn-based role-playing game made using only javascript and HTML5's Canvas."
-            },
-            {
                 name: "Video Game Backlog Tracker",
                 pic: vgPic,
                 site: "https://guarded-temple-25512.herokuapp.com/",
                 demo: "https://www.youtube.com/watch?v=pkllnDsc7iU",
                 github: "https://github.com/nrymarz/vg-backlog-tracker",
-                description: "This is a web-app that allows users to keep track of games they want to play. It takes inspiration from Rawg.io and uses their API for finding information about games. Users can search for games and add them to their backlog. In their backlog they can categorize games as not played, started or completed."
+                description: "This is a web-app that allows users to keep track of games they want to play. It takes inspiration from Rawg.io and uses their API for finding information about games. Users can search for games and add them to their backlog. In their backlog they can categorize games as not played, started or completed.",
+                technologies: ["React","Redux","Ruby On Rails","Thunk", "JWT"]
+            },
+            {
+                name: "Dragon Adventure RPG",
+                pic: rpgPic,
+                site: "https://dragon-adventure.herokuapp.com/",
+                demo: "https://www.youtube.com/watch?v=ZhbnOaCcE2E",
+                github: "https://github.com/nrymarz/js-dragon-rpg",
+                description: "This is a simple turn-based role-playing game made using only javascript and HTML5's Canvas.",
+                technologies: ["JavaScript", "HTML5 Canvas"]
+
             },
             {
                 name: "Twitch.tv Random Stream Finder",
@@ -33,7 +35,8 @@ export default class Projects extends Component{
                 site: "https://randomstreamfinder.herokuapp.com/",
                 demo: "https://www.youtube.com/watch?v=EoeyYE8D6IU",
                 github: "https://github.com/nrymarz/Twitch-Random-Stream-Finder",
-                description: "This is a web-application that uses the Twitch.tv API to find random streams. It allows users to view streams, login and post recommendations for streamers that they find using this app."
+                description: "This is a web-application that uses the Twitch.tv API to find random streams. It allows users to view streams, login and post recommendations for streamers that they find using this app.",
+                technologies: ["Ruby On Rails", "Oauth"]
             }
         ],
     }
@@ -43,20 +46,14 @@ export default class Projects extends Component{
                 <Col xl={9} className="m-auto">
                     <ScrollAnimation animateIn="animate__fadeInDown"><h1 className="py-3">Projects</h1> </ScrollAnimation>
                     <ScrollAnimation animateIn="animate__fadeIn">
-                        <Carousel interval={7000}>
-                            <Carousel.Item >
-                                <Project project={this.state.projects[0]} height={this.state.height}/>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <Project project={this.state.projects[1]} height={this.state.height} />
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <Project project={this.state.projects[2]} height={this.state.height} />
-                            </Carousel.Item>
-                        </Carousel>
+                        {this.renderProjects()}
                     </ScrollAnimation>
                 </Col>
             </Container>
         )
+    }
+
+    renderProjects(){
+        return this.state.projects.map( (proj,idx) => <Project project={proj} key={proj.name} idx={idx}/>)
     }
 }
