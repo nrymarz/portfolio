8897bd9c4ec0a797749495a5266700e0a1af3f5b
Nathan Rymarz <nrymarz@gmail.com>
2021-04-15 20:23:20 -0500

    added border and changed lines of project descriptions

--- src/components/project.js
+++ src/components/project.js
@@ -5,9 +5,8 @@ import Col from 'react-bootstrap/Col'
 export default function Project({project}){
     return(
         <Row>
-            <Col xs={12} xl={6} className="d-flex project-about">
-                <div className="d-flex flex-column text-center mx-auto w-100">
-                    <br/>
+            <Col xs={12} xl={6} className="d-flex">
+                <Col xs={12} xl={10} className="d-inline-flex flex-column text-center mx-auto project-about">
                     <h3 className="my-auto">{project.name}</h3>
                     <br/>
                     <p className="my-auto">{project.description}</p>
@@ -17,7 +16,7 @@ export default function Project({project}){
                         <Breadcrumb.Item href={project.demo} target="_blank">Youtube Demo</Breadcrumb.Item>
                         <Breadcrumb.Item href={project.github} target="_blank" className="mr-auto">Github</Breadcrumb.Item>
                     </Breadcrumb>
-                </div>
+                </Col>
             </Col>
             <Col className='d-flex'> <img src={project.pic} alt={project.name} className="mx-auto project-pic"/> </Col>
         </Row>
