5262e8c05c74c7a4f4a0e752e0f546e25f10ac9d
Nathan Rymarz <nrymarz@gmail.com>
2021-04-16 14:14:49 -0500

    fixed contact info to be flush left

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,4 @@
 import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
 import gmail from '../assets/icons/gmail_icon.png'
 import linkedIn from '../assets/icons/linkedIn.png'
 import resumeIcon from '../assets/icons/resume_icon.png'
@@ -7,14 +6,14 @@ import ScrollAnimation from 'react-animate-on-scroll'
 export default function Contact(){
     return(
         <Container fluid id="contact">
-            <Col className="m-auto text-center text-white">
+            <div className="m-auto d-inline-block text-white">
                 <ScrollAnimation animateIn="animate__fadeIn">
                     <h1>Contact Information</h1>
                     <h3><img src={gmail} alt="gmail icon" width="50" height="50"/> nrymarz@gmail.com</h3>
                     <h3> <img src={linkedIn} alt="LinkedIn icon" width="50" height="50"/><a href="https://www.linkedIn.com/in/nathan-rymarz" target="_blank" rel="noreferrer"> LinkedIn Profile</a> </h3>
                     <h3><img src={resumeIcon} alt="resume icon" width="50" height="50"/><a href="./NathanRymarz.pdf" target="_blank" rel="noreferrer"> Resume PDF</a></h3>
                 </ScrollAnimation>
-            </Col>
+            </div>
         </Container>  
     )
 }
